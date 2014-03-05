8679d3c71d86f899994c1058bf9d6e8c5866ab1e
ignorabimus <watanabe@smile.plala.or.jp>
2014-03-05 23:42:08 +0900

    Revert "Fix a memory leak possibility of DO macro."
    This reverts commit 8b32ce80fc537334c54699aa60617068b1aca736.

--- src/init.scm
+++ src/init.scm
@@ -474,27 +474,28 @@
 (macro do
   (lambda (do-macro)
     (apply (lambda (do vars endtest . body)
-             `(letrec ((do-loop
-                         (lambda ,(map (lambda (x)
-                                         (if (pair? x) (car x) x))
-                                    `,vars)
-                           (if ,(car endtest)
-                             (begin ,@(cdr endtest))
-                             (begin
-                               ,@body
-                               (do-loop
-                                 ,@(map (lambda (x)
-                                          (cond
-                                            ((not (pair? x)) x)
-                                            ((< (length x) 3) (car x))
-                                            (else (car (cdr (cdr x))))))
-                                     `,vars)))))))
-                (do-loop
-                  ,@(map (lambda (x)
-                           (if (and (pair? x) (cdr x))
-                             (car (cdr x))
-                             '()))
-                      `,vars))))
+             (let ((do-loop (gensym)))
+               `(letrec ((,do-loop
+                           (lambda ,(map (lambda (x)
+                                           (if (pair? x) (car x) x))
+                                      `,vars)
+                             (if ,(car endtest)
+                               (begin ,@(cdr endtest))
+                               (begin
+                                 ,@body
+                                 (,do-loop
+                                   ,@(map (lambda (x)
+                                            (cond
+                                              ((not (pair? x)) x)
+                                              ((< (length x) 3) (car x))
+                                              (else (car (cdr (cdr x))))))
+                                       `,vars)))))))
+                  (,do-loop
+                    ,@(map (lambda (x)
+                             (if (and (pair? x) (cdr x))
+                               (car (cdr x))
+                               '()))
+                        `,vars)))))
       do-macro)))
 
 ;;;; generic-member

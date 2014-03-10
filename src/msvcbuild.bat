5aa53dcdcfd7df7c8d91ef939c662c7a746e0301
ignorabimus <watanabe@smile.plala.or.jp>
2014-03-11 00:28:07 +0900

    Simplify msvcbuild.bat by only using USE_EMB_INIT=1.

--- src/msvcbuild.bat
+++ src/msvcbuild.bat
@@ -3,31 +3,22 @@
 @set TSLINK=link /nologo
 @set TSLIB=lib /nologo
 
+%TSCOMPILE% /MT scheme.c dynload.c
+%TSLINK% /out:tinyscheme.exe scheme.obj dynload.obj
+
+tinyscheme.exe mk_init_scm.scm
+
 @if "%1" == "static" goto STATIC
 @if not exist bin\ (
   mkdir bin
 )
 
-@if "%1" == "emb_init" (
-  %TSCOMPILE% /MT scheme.c dynload.c
-  %TSLINK% /out:tinyscheme.exe scheme.obj dynload.obj
+%TSCOMPILE% /DUSE_EMB_INIT=1 /MT scheme.c dynload.c
+%TSLINK% /out:bin\tinyscheme.exe scheme.obj dynload.obj
 
-  tinyscheme.exe mk_init_scm.scm
+%TSCOMPILE% /DUSE_EMB_INIT=1 /MDd scheme.c dynload.c
+%TSLINK% /out:bin\tinyscheme_d.exe scheme.obj dynload.obj
 
-  %TSCOMPILE% /DUSE_EMB_INIT=1 /MT scheme.c dynload.c
-  %TSLINK% /out:bin\tinyscheme.exe scheme.obj dynload.obj
-
-  %TSCOMPILE% /DUSE_EMB_INIT=1 /MDd scheme.c dynload.c
-  %TSLINK% /out:bin\tinyscheme_d.exe scheme.obj dynload.obj
-
-  del tinyscheme.* init_scm.h
-) else (
-  %TSCOMPILE% /MT scheme.c dynload.c
-  %TSLINK% /out:bin\tinyscheme.exe scheme.obj dynload.obj
-
-  %TSCOMPILE% /MDd scheme.c dynload.c
-  %TSLINK% /out:bin\tinyscheme_d.exe scheme.obj dynload.obj
-)
 @goto END
 
 :STATIC
@@ -35,18 +26,11 @@
   mkdir lib
 )
 
-%TSCOMPILE% /MT scheme.c dynload.c
-%TSLINK% /out:tinyscheme.exe scheme.obj dynload.obj
-
-tinyscheme.exe mk_init_scm.scm
-
-%TSCOMPILE% /DSTANDALONE=0 /DUSE_EMB_INIT=1 /MT scheme.c dynload.c
+%TSCOMPILE% /DUSE_EMB_INIT=1 /DSTANDALONE=0 /MT scheme.c dynload.c
 %TSLIB% /out:lib\tinyscheme.lib scheme.obj dynload.obj
 
-%TSCOMPILE% /DSTANDALONE=0 /DUSE_EMB_INIT=1 /MDd scheme.c dynload.c
+%TSCOMPILE% /DUSE_EMB_INIT=1 /DSTANDALONE=0 /MDd scheme.c dynload.c
 %TSLIB% /out:lib\tinyscheme_d.lib scheme.obj dynload.obj
 
-del tinyscheme.* init_scm.h
-
 :END
-del scheme.obj dynload.obj
+del scheme.obj dynload.obj tinyscheme.* init_scm.h

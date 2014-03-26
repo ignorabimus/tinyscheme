a9f982684da388d140c58af4eafd2e1cbd678873
ignorabimus <watanabe@ignorabimus.info>
2014-03-27 02:51:18 +0900

    Modify hash_fn()'s mixing step to 'BP Hash' algorithm.

--- src/scheme.c
+++ src/scheme.c
@@ -2163,13 +2163,9 @@ int eqv(pointer a, pointer b) {
 static int hash_fn(const char *key, int table_size)
 {
   unsigned int hashed = 0;
-  const char *c;
-  int bits_per_int = sizeof(unsigned int)*8;
 
-  for (c = key; *c; c++) {
-    /* letters have about 5 bits in them */
-    hashed = (hashed<<5) | (hashed>>(bits_per_int-5));
-    hashed ^= *c;
+  while (*key) {
+    hashed = (hashed << 7) ^ *key++;
   }
   return hashed % table_size;
 }
